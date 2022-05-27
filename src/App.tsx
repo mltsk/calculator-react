b1c6a8825d196ae5ba311007fbb1693187cea7b3
Dmitrii Maletskii <mltsk94@gmail.com>
2022-05-27 16:31:13 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.sdf
         </p>
         <a
           className="App-link"
