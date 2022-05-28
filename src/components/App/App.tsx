1b7729c44176d22728069b42d698310a89fb6cdc
Dmitrii Maletskii <mltsk94@gmail.com>
2022-05-28 16:03:50 +0300

    implement typing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
+import { KEYS, SPECIAL_KEYS } from '../../constants/constants';
 import CalculatorContainer from '../CalculatorContainer/CalculatorContainer';
 import CalculatorHeader from '../CalculatorHeader/CalculatorHeader';
 import Controls from '../Controls/Controls';
@@ -10,6 +11,30 @@ function App() {
     null
   );
   const [result, setResult] = useState<number>(0);
+
+  const keyDownHandler = (event: {
+    key: string | null | undefined;
+    preventDefault: () => void;
+  }) => {
+    if (!event.key) return;
+    if (KEYS.includes(event.key)) {
+      setCalculation((prev) => (prev ? prev + event.key : event.key));
+    } else if (event.key === SPECIAL_KEYS.ENTER) {
+      event.preventDefault();
+      console.log('calculation...');
+    } else if (event.key === SPECIAL_KEYS.ESCAPE) {
+      setCalculation(null);
+      setResult(0);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler);
+    };
+  });
 
   return (
     <Layout>
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import cn from 'classnames';
 import React from 'react';
 
+import { SPECIAL_BUTTONS } from '../../constants/constants';
 import { ButtonType } from '../../ts/enums';
 
 import styles from './Button.module.scss';
@@ -14,9 +15,22 @@ interface IButton {
 }
 
 const Button = ({ value, type, setCalculation }: IButton) => {
+  const handlerClick = (event: React.MouseEvent<HTMLElement>) => {
+    if (!(event.target instanceof HTMLElement)) return;
+    const char = event.target.textContent;
+
+    if (char === SPECIAL_BUTTONS.RESULT) {
+      console.log('calculation...');
+    } else if (char === SPECIAL_BUTTONS.DELETE) {
+      setCalculation(null);
+    } else {
+      setCalculation((prev) => (prev ? prev + char : char));
+    }
+  };
 
   return (
     <button
+      onClick={handlerClick}
       className={cn({
         [styles.button]: true,
         [styles.result]: type === ButtonType.Result,
